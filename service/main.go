49d0cf407895fbe11d58efc1df2168791e8a4828
leoliangliwen <leoliangliwen@gmail.com>
2018-06-09 11:59:05 -0400

    change ip

--- service/main.go
+++ service/main.go
@@ -18,7 +18,7 @@ const (
       //PROJECT_ID = "around-xxx"
       //BT_INSTANCE = "around-post"
       // Needs to update this URL if you deploy it to cloud.
-      ES_URL = "http://35.237.140.200:9200"
+      ES_URL = "http://104.196.203.87:9200"
 )
 
 type Location struct {
